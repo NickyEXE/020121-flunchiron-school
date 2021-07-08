5aabbb9a5c7605fdaa248984da25b0bcc2f25df9
NickyEXE <dovern42@gmail.com>
2021-07-08 19:46:50 -0400

    adds login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, :email, uniqueness: true
+  validates :username, :email, presence: true
+  validates :zip_code, numericality: {greater_than_or_equal_to: 10001, less_than_or_equal_to: 99999}
 end
